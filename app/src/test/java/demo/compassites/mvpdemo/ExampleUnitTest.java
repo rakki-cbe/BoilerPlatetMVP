42561f843e7cbd6e4e62d0448e798e34ead1709a
Radhakrishna <rakki.bsc2009@gmail.com>
2017-04-13 16:49:41 +0530

    igonore file and readme

--- app/src/test/java/demo/compassites/mvpdemo/ExampleUnitTest.java
+++ app/src/test/java/demo/compassites/mvpdemo/ExampleUnitTest.java
@@ -1,17 +1,42 @@
 package demo.compassites.mvpdemo;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.assertEquals;
+import demo.compassites.mvpdemo.feature.Products.presenter.ProductListPresenter;
+import demo.compassites.mvpdemo.feature.Products.view.contract.ListView;
+
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Products local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+    ProductListPresenter list;
+    @Mock
+    ListView listView;
+
+    @Before
+    public void setUp() {
+        list = new ProductListPresenter(listView);
+        assertNotEquals(list, null);
+    }
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+       /* final List<Product> products=new ArrayList<>();
+           when(list.getProductsList()).thenAnswer(
+                new Answer() {
+                    public Object answer(InvocationOnMock invocation) {
+                        ListView callback =
+                                (ListView) invocation.getArguments()[1];
+                        callback.setProductList(products);
+                    }
+                });*/
     }
 }
